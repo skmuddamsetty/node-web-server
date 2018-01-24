99bce93dc1c6cf3a97dd324ae96db748e0db39d9
skmuddamsetty <muddamsetty.santhosh@gmail.com>
2018-01-23 20:42:42 -0500

    Initial Commit

---
+++ server.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const hbs = require('hbs');
+var app = express();
+const fs = require('fs');
+
+hbs.registerPartials(__dirname+'/views/partials');
+app.set('view engine','hbs');
+
+
+app.use((req,res,next)=>{
+  var now = new Date().toString();
+  var log = `${now}: ${req.method} ${req.url}`;
+  console.log(log);
+  fs.appendFile('server.log',log+'\n',(error)=>{
+    console.log(error);
+  });
+  next();
+});
+
+app.use((req,res,next)=>{
+  res.render('maintenance.hbs');
+});
+
+app.use(express.static(__dirname+'/public'));
+
+hbs.registerHelper('getCurrentYear',()=>{
+  return new Date().getFullYear();
+});
+hbs.registerHelper('screamIt',(text)=>{
+  return text.toUpperCase();
+});
+
+app.get('/',(req,res)=>{
+  res.render('home.hbs',{
+    welcomeMessage: 'Welcome',
+    pageTitle: 'Welcome to my website'
+  });
+});
+
+app.get('/about',(req,res)=>{
+  res.render('about.hbs',{
+    pageTitle: 'About Page'
+  });
+});
+
+app.listen(8000,()=>{
+  console.log('Server up and running on port 8000');
+});
